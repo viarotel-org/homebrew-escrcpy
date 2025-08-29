1928ace837578c69d99a9d390850e9706ed8d99d
viarotel <viarotel@qq.com>
2025-08-29 17:37:17 +0800

    fix: 🐛 Sha256 error

--- Casks/escrcpy.rb
+++ Casks/escrcpy.rb
@@ -3,7 +3,7 @@ cask "escrcpy" do
 
   version "1.30.2"
   sha256 arm:   "211c9dd5f8c2bbd394b70efeccebc360bbee48a1ed430a1e6ac5e768c91c3855",
-         intel: "211c9dd5f8c2bbd394b70efeccebc360bbee48a1ed430a1e6ac5e768c91c3855"
+         intel: "6e842a13165b63b691feab24b42502e48c556aaa4e53cc430093687b0faafa3d"
 
   url "https://github.com/viarotel-org/escrcpy/releases/download/v#{version}/Escrcpy-#{version}-mac-#{arch}.dmg"
   name "Escrcpy"
