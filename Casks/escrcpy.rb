421646eeed1248da2714c90d538716734e732c79
viarotel <38282758+viarotel@users.noreply.github.com>
2026-04-02 06:40:21 +0000

    escrcpy 2.7.3

--- Casks/escrcpy.rb
+++ Casks/escrcpy.rb
@@ -1,9 +1,9 @@
 cask "escrcpy" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.7.0"
-  sha256 arm:   "923fced73557ac20ba0b22ffbc94b88c969fbb05b75d1c275a3e822a6b8b6984",
-         intel: "4877cde7831aae689ec9db42f31df3b1ba60aa24fa89501e25725af3a6ca8a1c"
+  version "2.7.3"
+  sha256 arm:   "9e0acb0ca1edb0939e005ba2b619677469958081698b9b3022d254f6c0ea8ea8",
+         intel: "df3906ac5be553752a85b50e53318cc524557e0afb27342d9334aadbf0d5c5a3"
 
   url "https://github.com/viarotel-org/escrcpy/releases/download/v#{version}/Escrcpy-#{version}-mac-#{arch}.dmg"
   name "Escrcpy"
